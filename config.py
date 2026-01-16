c1e9e217e7819f065a9318fe9758969aa3216953
Arjun Tandon <113589883+truelearnerarjun@users.noreply.github.com>
2026-01-16 06:47:06 +0000

    stopped tracking ignored files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-customhost = os.environ.get('DB_HOST', 'employee.cn8u6eqimjly.us-east-2.rds.amazonaws.com')
-customuser = os.environ.get('DB_USER', 'admin')
-custompass = os.environ.get('DB_PASS', 'Arjun123')
-customdb = os.environ.get('DB_NAME', 'employee')
-custombucket = os.environ.get('S3_BUCKET', 'add-emp--123')
-customregion = os.environ.get('AWS_REGION', 'us-east-2')
-customtable = os.environ.get('DYNAMODB_TABLE', 'emp_image_table')
